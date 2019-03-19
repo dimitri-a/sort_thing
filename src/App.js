8d9ec954ee5f524268da7c361ecc800ab1267d6a
Dimitri C <couwenberg@gmail.com>
2019-03-20 10:25:14 +1100

    quality working again

--- src/App.js
+++ src/App.js
@@ -9,41 +9,48 @@ class App extends Component {
   render() {
 
     console.log('start render', help)
-  
-  
-  var order = {
-    model: {
-      Commercial: 1,
-      Agribusiness: 2
-    },
-    type: {
-      Historical: 1,
-      Actual: 2,
-      Projected: 3,
-      Proforma: 4
-    },
-    Actual: {
-      Unqualified: 1,
-      Qualified: 2,
-      Audited: 3
-    },
-    Projected: {
-      Projection: 1,
-      Qualified: 2,
-      Audited: 3
-    },
-    BOTTOM: Infinity
-  };
-  
-  let result = myData.sort(
-    (a, b) => {
-      return order.model[a.model] - order.model[b.model] || 
-      order.type[a.type] - order.type[b.type] ||
-      order.type[a.type][a.quality] - order.type[b.type][b.quality]
-    }
-  );
-  
-  console.log(result.map((obj) => obj.id))
+
+
+    var order = {
+      model: {
+        Commercial: 1,
+        Agribusiness: 2
+      },
+      type: {
+        Historical: 1,
+        Actual: 2,
+        Projected: 3,
+        Proforma: 4
+      },
+      Actual: {
+        Unqualified: 1,
+        Qualified: 2,
+        Audited: 3
+      },
+      Projected: {
+        Projection: 1,
+        Qualified: 2,
+        Audited: 3
+      },
+      BOTTOM: Infinity
+    };
+
+    let result = myData.sort(
+      (a, b) => {
+        console.log('a.startDate', a.startDate)
+        console.log('b.startDate', b.startDate)
+        const aDate = new Date(a.startDate);
+        const bDate = new Date(b.startDate);
+        const diff = Math.abs(aDate.getTime() - bDate.getTime())
+        console.log('diff', diff)
+        return order.model[a.model] - order.model[b.model] ||
+          order.type[a.type] - order.type[b.type] ||
+          diff ||
+          order.type[a.type][a.quality] - order.type[b.type][b.quality]
+      }
+    );
+
+    console.log(result.map((obj) => obj.id))
 
 
 
